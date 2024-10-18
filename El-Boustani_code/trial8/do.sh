07f7fa36224e4bd78d5a6225f79968a313448148
KIST-LCNP <soonho.eric.kim@gmail.com>
2024-10-18 16:40:37 -0400

    clean

--- El-Boustani_code/trial8/do.sh
+++
@@ -1,4 +0,0 @@
-python LR.py 1.6 SOM 0.012
-python LR.py 1.7 SOM 0.012
-python LR.py 1.8 SOM 0.012
-python LR.py 1.9 SOM 0.012
--- El-Boustani_code/trial8/do2.sh
+++
@@ -1,4 +0,0 @@
-python LR.py 1.6 PV 0.015
-python LR.py 1.7 PV 0.015
-python LR.py 1.8 PV 0.015
-python LR.py 1.9 PV 0.015
